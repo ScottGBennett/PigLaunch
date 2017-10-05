8fcee6b3e11a7cdb4c68a7939ae026c26b647528
Austin Corley <arcorley528@gmail.com>
2017-10-05 08:12:28 -0700

    Add files via upload

---
+++ Assets/Scripts/FollowPig.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPig : MonoBehaviour {
+
+    public Transform pig;
+    public Transform farLeft;
+    public Transform farRight;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 newPosition = transform.position;
+        newPosition.x = pig.position.x;
+        //newPosition.x = Mathf.Clamp(newPosition.x, newPosition.x, farRight.position.x); //set the values
+        newPosition.x = Mathf.Clamp(newPosition.x, farLeft.position.x, farRight.position.x);
+        Debug.Log("Pig position: " + pig.position.x);
+        Debug.Log("Far left: " + farLeft.position.x);
+        Debug.Log("Far right: " + farRight.position.x);
+        Debug.Log("");
+        transform.position = newPosition;
+
+        //move the left marker each frame 3 units behind the pig
+        newPosition.x = pig.position.x - 3f; 
+        farLeft.position = newPosition;
+
+        //move the right marker each frame 7 units in front of the pig
+        newPosition.x = pig.position.x + 7f;
+        farRight.position = newPosition;
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float maxStretch = 3.0f;
+    public LineRenderer catapultLineFront;
+    public LineRenderer catapultLineBack;
+
+    private SpringJoint2D spring;
+    private Transform catapult;
+    private Rigidbody2D rb2d;
+    private Ray leftCatapultToProjectile;
+    private CircleCollider2D circle;
+    private float circleRadius;
+    private bool clickedOn;
+    private Vector2 prevVelocity;
+
+    void Awake()
+    {
+        spring = GetComponent<SpringJoint2D>();
+        rb2d = GetComponent<Rigidbody2D>();
+        catapult = spring.connectedBody.transform;
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spring != null)
+        {
+            if (!rb2d.isKinematic && prevVelocity.sqrMagnitude > rb2d.velocity.sqrMagnitude)
+            {
+                Destroy(spring);
+                rb2d.velocity = prevVelocity;
+            }
+
+            if (!clickedOn)
+                prevVelocity = rb2d.velocity;
+        }
+
+        else
+        {
+            catapultLineFront.enabled = false;
+            catapultLineBack.enabled = false;
+        }
+	}
+
+    private void OnMouseDown()
+    {
+        spring.enabled = false;
+        clickedOn = true;
+    }
+
+    private void OnMouseUp()
+    {
+        spring.enabled = true;
+        rb2d.isKinematic = false;
+        rb2d.angularDrag = 1f;
+        rb2d.drag = 0.3f;
+        rb2d.gravityScale = 1f;
+        rb2d.mass = 5;
+        clickedOn = false;
+    }
+
+}
